482479afb330001b472964c854963afb7926f079
David Choi <daaave@gmail.com>
2018-10-24 03:48:18 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -17,9 +17,17 @@ let codeIndex = 0;
 function onKeyDownHandler(e){
   let key = e.key;
   
+  let targetKey = codes[codeIndex];
+  if(key === targetKey){
+    codeIndex++;
+    if(codeIndex === codes.length){
+      alert("Hurray!");
+      codeIndex = 0;
+    }
+  }
 }
 
 function init() {
   // your code here
-  
+  document.onkeydown = onKeyDownHandler;
 }
